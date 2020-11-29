bc6d4abd77014b40a1f51f2404f7fe7cd6eb87a6
John Obla <john-obla@live.co.uk>
2020-11-29 23:00:51 +0000

    Added ejs-mate boilerplate

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const ejsMate = require("ejs-mate");
 const path = require('path');
 const Campground = require("./models/campground");
-const methodOverride = require('method-override');
 
 
 mongoose.connect('mongodb://localhost:27017/yelpCamp', {
@@ -18,6 +19,9 @@ db.once("open", () => {
 });
 
 const app = express();
+
+app.engine("ejs", ejsMate);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "views"));
 
