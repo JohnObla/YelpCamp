af5ef299d3f138c9cad5803503f21839488aebdd
John Obla <john-obla@live.co.uk>
2020-11-29 15:34:51 +0000

    Setup details page

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+
+    res.render('campgrounds/details', campground);
+});
+
 app.listen('3000', () => {
     console.log("Serving on port 3000");
 });
