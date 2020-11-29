431a1b309c980e85c4dc9592ce1974f59baac08e
John Obla <john-obla@live.co.uk>
2020-11-29 13:49:27 +0000

    Basic setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const app = express();
+const path = require('path');
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html')
+
+app.set('views', path.join(__dirname, "views"));
+
+app.get('/', (req, res) => {
+    // res.send("Hello from yelp camp!");
+    res.render('home');
+});
+
+app.listen('3000', () => {
+    console.log("Serving on port 3000");
+})
