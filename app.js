8a00725066382b759588719ccb9750b252420d46
John Obla <john-obla@live.co.uk>
2020-12-04 18:27:08 +0000

    Added very basic error handling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const methodOverride = require('method-override');
 const ejsMate = require("ejs-mate");
 const path = require('path');
 const Campground = require("./models/campground");
+const morgan = require("morgan");
 
 
 mongoose.connect('mongodb://localhost:27017/yelpCamp', {
@@ -28,6 +29,8 @@ app.set('views', path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+app.use(morgan("dev"));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -41,11 +44,16 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
+app.post('/campgrounds', async (req, res, next) => {
+    try {
+        const campground = new Campground(req.body.campground);
+        await campground.save();
 
-    res.redirect(`campgrounds/${campground.id}`)
+        res.redirect(`campgrounds/${campground.id}`);
+    } catch (e) {
+        next(e);
+    }
+
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
@@ -62,11 +70,16 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
     res.render('campgrounds/edit', campground);
 });
 
-app.put('/campgrounds/:id', async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndUpdate(id, req.body.campground, { runValidators: true, useFindAndModify: false });
+app.put('/campgrounds/:id', async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        await Campground.findByIdAndUpdate(id, req.body.campground, { runValidators: true, useFindAndModify: false });
 
-    res.redirect(`/campgrounds/${id}`);
+        res.redirect(`/campgrounds/${id}`);
+    } catch (error) {
+        next(error);
+    }
+
 });
 
 app.get('/campgrounds/:id/delete', async (req, res) => {
@@ -82,6 +95,10 @@ app.delete('/campgrounds/:id', async (req, res) => {
 
     res.redirect('/campgrounds')
 });
+
+app.use((err, req, res, next) => {
+    res.send("Oh boy, something went wrong!");
+})
 
 app.listen('3000', () => {
     console.log("Serving on port 3000");
