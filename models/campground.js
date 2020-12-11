acc27a7193984e564fc836deac0ddbd3dbdf2b86
John Obla <john-obla@live.co.uk>
2020-12-11 15:00:54 +0000

    Deleting campground also deletes reviews

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const review = require('./review');
 const Schema = mongoose.Schema;
 
 const campgroundSchema = new Schema({
@@ -11,6 +12,12 @@ const campgroundSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "Review"
     }]
+});
+
+campgroundSchema.post('findOneAndDelete', async (campground) => {
+    if (campground) {
+        await review.deleteMany({ _id: { $in: campground.reviews } });
+    }
 });
 
 module.exports = mongoose.model('Campground', campgroundSchema);
