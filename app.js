5d5daa8ff224e541c49285c6fb6bad37c1b5b965
John Obla <john-obla@live.co.uk>
2020-12-12 00:12:10 +0000

    Added comments

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const ejsMate = require("ejs-mate");
 const path = require("path");
 const morgan = require("morgan");
 
+// Custom routes
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
 
+// Custom Error
 const ExpressError = require("./utils/ExpressError");
 
+// Database setup
 mongoose.connect("mongodb://localhost:27017/yelpCamp", {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -23,18 +26,23 @@ db.once("open", () => {
   console.log("Database connected");
 });
 
+// Defines app
 const app = express();
 
+// Ejs template setup
 app.engine("ejs", ejsMate);
-
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 
+// Views and Public setup
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Installed middleware setup
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -42,16 +50,19 @@ app.get("/", (req, res) => {
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
 
+// Throws 404 if no route found
 app.all("*", (req, res, next) => {
   return next(new ExpressError("Page Not Found", 404));
 });
 
+// Renders error if found
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
   if (!err.message) err.message = "Oh No, Something Went Wrong!";
   res.status(statusCode).render("error", { err });
 });
 
+// Starts server
 app.listen("3000", () => {
   console.log("Serving on port 3000");
 });
