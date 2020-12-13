23edda797bcd8a12d1e855f27081a405e920d366
John Obla <john-obla@live.co.uk>
2020-12-13 22:36:06 +0000

    Can register user

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 
 // Custom routes
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/users");
 
 // Models
 const User = require("./models/user");
@@ -91,8 +92,9 @@ app.get("/fakeuser", async (req, res) => {
   res.send(newUser);
 });
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 // Throws 404 if no route found
 app.all("*", (req, res, next) => {
---
+++ routes/users.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
+
+router.get("/register", (req, res) => {
+  res.render("users/register");
+});
+
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+
+      const user = new User({
+        email,
+        username,
+      });
+      const registeredUser = await User.register(user, password);
+
+      req.flash("success", "Welcome to Yelp Camp!");
+      res.redirect("/campgrounds");
+    } catch (error) {
+      req.flash("error", error.message);
+      return res.redirect("/register");
+    }
+  })
+);
+
+module.exports = router;
